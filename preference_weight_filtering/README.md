0148f6bb01a2731cd40392fb74a763293b0916bf
Ross Geurts <rmgeurtd@gmail.com>
2026-01-30 22:43:09 +0100

    Merge preference weight filtering branch as subdirectory

---
+++ preference_weight_filtering/README.md
@@ -0,0 +1,7 @@
+### filter_data_preference.py : For filtering preference weights in the datasets
+
+Before use: ensure you filter based on the intented lower and higher bound of preference weights, all that fall in between the bound will be removed. Change `path` and `new_path` as desired.
+
+### get_results_filtered.py : Obtain the average NSW for your desired seeds over all results
+
+### visualize_data.ipynb : Visualise the data distribution of your desired dataset
---
+++ preference_weight_filtering/get_results_filtered.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+
+results = []
+
+envs = ["MO-Hopper-v2", "MO-Walker2d-v2", "MO-Swimmer-v2", "MO-HalfCheetah-v2", "MO-Ant-v2"]
+qualities = ["expert", "amateur"]
+betas = [1, 0.1, 0.01, 0.001, 0.0001]
+seeds = [1, 2, 3, 4, 5]
+
+path = "results/"
+files = os.listdir(path)
+
+for file in files:
+    folder_path = os.path.join(path, file)
+
+    file = file.split("_")
+    env = file[3]
+    quality = file[4]
+    beta = file[8][4:]
+    seed = file[9][4:]
+
+    npy_path = os.path.join(folder_path, "eval", "normalized_returns_step_100000.npy")
+    normalized_returns = np.load(npy_path)
+    nsw_score = np.mean(np.sum(np.log(normalized_returns), axis=1))
+
+    results.append({
+        "env": env,
+        "quality": quality,
+        "beta": beta,
+        "seed": seed,
+        "nsw_score": nsw_score
+    })
+
+for env in envs:
+    for quality in qualities:
+        for beta in betas:
+            nsw_scores = []
+
+            for seed in seeds:
+                for r in results:
+                    if (
+                        r["env"] == env
+                        and r["quality"] == quality
+                        and float(r["beta"]) == beta
+                        and int(r["seed"]) == seed
+                    ):
+                        nsw_scores.append(r["nsw_score"])
+            
+            if len(nsw_scores) == 0:
+                continue
+
+            nsw_scores = np.array(nsw_scores)
+            mean_nsw = nsw_scores.mean()
+            std_nsw = nsw_scores.std()
+
+            print(f"Environment: {env}, Quality: {quality}, Beta: {beta}, NSW: {mean_nsw:.4f}, STD: {std_nsw:.4f}")
